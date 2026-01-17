79e536b548b837f19d6ab9052a2287ba298d374d
Arthur508555 <arthuroliveiragoncalves826@gmail.com>
2026-01-17 19:58:00 -0300

    Update InfinityYieldObuscafor.lua with new config loading

---
+++ InfinityYieldObuscafor.lua
@@ -0,0 +1,9 @@
+--[[   █████╗ ██████╗ ████████╗██╗  ██╗██╗   ██╗██████╗ ██╗████████╗ ██████╗ 
+      ██╔══██╗██╔══██╗╚══██╔══╝██║  ██║██║   ██║██╔══██╗██║╚══██╔══╝██╔═══██╗
+      ███████║██████╔╝   ██║   ███████║██║   ██║██████╔╝██║   ██║   ██║   ██║
+      ██╔══██║██╔══██╗   ██║   ██╔══██║██║   ██║██╔══██╗██║   ██║   ██║   ██║
+      ██║  ██║██║  ██║   ██║   ██║  ██║╚██████╔╝██║  ██║██║   ██║   ╚██████╔╝
+      ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝    ╚═════╝  --]]
+
+local Yield = loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+local Config = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Arthur508555/InfinityYieldConfig/refs/heads/main/Config.lua"))()
